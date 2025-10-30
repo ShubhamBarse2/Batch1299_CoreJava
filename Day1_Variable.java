3f0ed85a63268ef55dc9872212c26d33f3615c26
ShubhamBarse2 <shubhambarse510@gmail.com>
2025-10-30 18:21:46 +0530

    Add files via upload

---
+++ Day1_Variable.java
@@ -0,0 +1,25 @@
+
+public class Day1_Variable {
+	
+	
+
+	public static void main(String[] args) {
+
+		// local variables
+
+		String stud_Name = "Ram";
+		int myAge = 24;
+		int rollNo = 123;
+		String email = "ram1996@gmail.com";
+		float marks = 78.56f;
+		boolean isPassed = true;             // 3>4
+
+		System.out.println(stud_Name);
+		System.out.println(myAge);
+		System.out.println(rollNo);
+		System.out.println(email);
+		System.out.println(marks);
+		System.out.println(isPassed);
+	}
+
+}
