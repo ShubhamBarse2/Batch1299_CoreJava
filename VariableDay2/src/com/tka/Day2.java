d4bf95081900070607bd66b7d319062f1b01db17
ShubhamBarse2 <shubhambarse510@gmail.com>
2025-10-31 18:06:51 +0530

    Add files via upload

---
+++ VariableDay2/src/com/tka/Day2.java
@@ -0,0 +1,18 @@
+package com.tka;
+
+public class Day2 {
+		
+	public static void main(String[] args) {
+	 
+	 System.out.println();
+	
+		
+	}
+	
+//	A 6
+//	B 0
+//	C not compiled
+//	D none of above
+	
+	
+}
