c6b36928183c888793cc293bd514170dcd51ed62
ShubhamBarse2 <shubhambarse510@gmail.com>
2025-11-04 22:48:00 +0530

    Add files via upload

---
+++ Day5.java
@@ -0,0 +1,22 @@
+package com.operators;
+
+public class Day5 {
+
+	public static void main(String[] args) {
+
+		// Comparison Operators
+		/*
+		 * > , < >= ,<= , == , !=
+		 */
+		int a = 3, b = 3;
+
+		System.out.println(a + " is Greater than " + b + " = " + (a > b));
+		System.out.println(a + " is Less Than " + b + " = " + (a < b));
+		System.out.println(a + " is Greater than Equals to " + b + " = " + (a >= b));
+		System.out.println(a + " is Less than Equals to " + b + " = " + (a <= b));
+		System.out.println(a + " is Equals to " + b + " = " + (a == b));
+		System.out.println(a + " is Not Equals to " + b + " = " + (a != b));
+
+	}
+
+}
---
+++ Day51.java
@@ -0,0 +1,30 @@
+package com.operators;
+
+public class Day51 {
+
+	public static void main(String[] args) {
+
+//	Logical Operator	&&    AND 
+
+//		System.out.println(true && true);    // true
+//		System.out.println(true && false);   // false
+//		System.out.println(false && true);   // false
+//		System.out.println(false && false);  // false 
+
+//		System.out.println(5>4 && 3<1);
+
+		
+		
+		// || OR 
+		
+//		System.out.println(true || true);  // true
+//		System.out.println(true || false); // true
+//		System.out.println(false || true); // true
+//		System.out.println(false || false); // false
+		
+		
+		// NOT  !
+
+	}
+
+}
