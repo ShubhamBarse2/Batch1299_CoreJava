bea0360e9799b1ba3d0142e93ce238d25d8b5469
ShubhamBarse2 <shubhambarse510@gmail.com>
2025-11-04 13:41:19 +0530

    Add files via upload

---
+++ Day4/src/com/demo/Day1.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+public class Day1 {
+
+	public static void main(String[] args) {
+
+		int a = 80, b = 40;
+		a++;
+		System.out.println(a+"D");
+//		System.out.println(a%b);
+//		System.out.println(2%1);
+//		System.out.println(12%11+3);
+
+	}
+
+}
