fa2e40cac20354eb81e5dd948abe440b69833be5
ShubhamBarse2 <shubhambarse510@gmail.com>
2025-11-06 18:15:17 +0530

    Add files via upload

---
+++ Day7.java
@@ -0,0 +1,18 @@
+package com.operators;
+
+public class Day7 {
+
+	public static void main(String[] args) {
+
+		// Assignment Operator
+
+		int a = 37;
+
+		System.out.println(a += 7); // 44
+		System.out.println(a -= 4); // 40 
+		System.out.println(a *= 2); // 80
+		System.out.println(a /= 6); // 13
+		System.out.println(a %= 8); // 5
+
+	}
+}
---
+++ Practise1.java
@@ -0,0 +1,22 @@
+package com.operators;
+
+public class Practise1 {
+
+	public static void main(String[] args) {
+		int i = 23;
+
+		i++; // 24
+
+		System.out.println(i); // 24
+		i--; // 23
+		System.out.println(i+=2); // 23 +2 = 25
+
+		System.out.println(i++); // 26
+		i--; // 25
+		System.out.println(i--); // 24
+		i++; // 25
+		i--; // 24
+		System.out.println(i); // 24
+	}
+
+}
