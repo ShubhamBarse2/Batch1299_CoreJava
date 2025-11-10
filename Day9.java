6c0b69bd7b56b357ae64231c7cff7a94969529fb
ShubhamBarse2 <shubhambarse510@gmail.com>
2025-11-10 18:25:45 +0530

    Add files via upload

---
+++ Day9.java
@@ -0,0 +1,29 @@
+package com.operators;
+
+public class Day9 {
+
+	public static void main(String[] args) {
+
+//		
+//		//  Integer  
+//		
+//		int decimal = 125;
+//		
+//		System.out.println(Integer.toBinaryString(decimal));
+
+//		System.out.println("Binary NO = "+Integer.toBinaryString(10));
+//		System.out.println(Integer.toBinaryString(23));
+
+//		System.out.println(14 & 43);
+
+//		System.out.println(11 | 19);
+
+//		System.out.println(~4); // - 4+1 = -5
+//		System.out.println(~-11); // - 11-1 = 10
+
+		System.out.println(14 >> 1);
+   		System.out.println(14 << 1);
+
+	}
+
+}
