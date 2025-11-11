e6c5118a2bf87e6a0d47a942b59334ca8d885b4a
ShubhamBarse2 <shubhambarse510@gmail.com>
2025-11-11 18:39:59 +0530

    Add files via upload

---
+++ If_Else_Day1.java
@@ -0,0 +1,20 @@
+package com.jbk;
+
+public class If_Else_Day1 {
+
+	public static void main(String[] args) {
+//		Q 1. Positive negative 
+
+//		int num = 0;
+//
+////	    -5 , -4 , -3 , -2 , -1 , 0 ,1 ,2 ,3 ,4 ,5 , 6 
+//
+//		if (num >= 0) {
+//			System.out.println(num + " Num is +ve");
+//		} else {
+//			System.out.println(num + " Num is -ve");
+//		}
+
+	}
+
+}
