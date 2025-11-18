893956b01097cf5a8242d167e9a7a4f7ea737047
ShubhamBarse2 <shubhambarse510@gmail.com>
2025-11-18 18:41:37 +0530

    Add files via upload

---
+++ LoopQ1.java
@@ -0,0 +1,31 @@
+package com.jbk;
+
+public class LoopQ1 {
+
+//	for(initialization ; condition ; updation){
+//
+//	}
+
+//	      -4, -3 , -2 , -1 , 0 ,1, 2, 3, 4 ,5
+
+	public static void main(String[] args) {
+
+//		for (int i = 51; i <= 100; i++) {
+//			System.out.println("Hello = " + i);
+//		}
+
+//		int i = 1;
+//		while (i <= 200) {
+//			System.out.println(i);
+//			i++;
+//		}
+
+		int i = 10;
+		do {
+			System.out.println(i);
+			i++;
+		} while (i <= 0);
+
+	}
+
+}
