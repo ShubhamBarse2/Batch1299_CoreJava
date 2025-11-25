ebda6170b282007bcad66d0fab10b3a8aa392f3d
ShubhamBarse2 <shubhambarse510@gmail.com>
2025-11-25 18:48:31 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,51 @@
+package com.oop;
+
+public class Student {
+
+//	global variables 
+
+	int age; // 0
+	String name; // null
+	String city;
+	float marks; // 0.0
+	boolean isPassed; // false
+	char grade;  // space
+
+	public static void main(String[] args) {
+
+//		Object Creation 
+//		syntax
+//		ClassName objName = new ClassName();
+
+		Student stud = new Student();
+
+//		int myAge = stud.age = 24;
+//		String myName = stud.name = "Rahul";
+//		String myCity = stud.city = "pune";
+//		float myMarks = stud.marks = 77.78f;
+//		char myGrade = stud.grade = 'C';
+//		boolean myResult = stud.isPassed = true;
+//
+//		System.err.println("Student Details...! ");
+//		System.out.println(" Student Name = " + myName);
+//		System.out.println(" Student Age = " + myAge);
+//		System.out.println(" Student City = " + myCity);
+//		System.out.println(" Student Marks = " + myMarks);
+//		System.out.println(" Student Grade = " + myGrade);
+//		System.out.println(" Student Result = " + myResult);
+
+//		Default values 
+
+//		System.err.println("Student Details...! ");
+
+//		System.err.println("Student Details...! ");
+//		System.out.println(" Student Name = " + stud.name);
+//		System.out.println(" Student Age = " + stud.age);
+//		System.out.println(" Student City = " + stud.city);
+//		System.out.println(" Student Marks = " + stud.marks);
+//		System.out.println(" Student Grade = " + stud.grade);
+//		System.out.println(" Student Result = " + stud.isPassed);
+
+	}
+
+}
