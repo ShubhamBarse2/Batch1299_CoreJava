c8b82862ed2df2d2c7624ae408a93e4eb9e5cccd
ShubhamBarse2 <shubhambarse510@gmail.com>
2025-11-26 18:50:38 +0530

    Add files via upload

---
+++ oop/src/com/oop/Employee.java
@@ -0,0 +1,12 @@
+package com.oop;
+
+public class Employee {
+
+// fields  // Variable // attributes 
+
+	public String empName;
+	public String city;
+	public int emp_Id;
+	public double salary;
+
+}
